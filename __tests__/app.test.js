52209220c7019dd075260a47d328cfcc40438d82
David <Rigby335@hotmail.com>
2022-08-10 14:10:35 -0700

    wrote test for redirect RED

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
 // const request = require('supertest');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/call?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'david_github_user',
+      email: 'totes-real@real.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
   afterAll(() => {
     pool.end();
