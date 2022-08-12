ad054e0eff304a4a37d06e2917c0bf34e17b0da9
David <Rigby335@hotmail.com>
2022-08-12 11:38:38 -0700

    added posts table/controller/test

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS gh_users;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE gh_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,3 +10,16 @@ CREATE TABLE gh_users (
     email TEXT,
     avatar TEXT
 );
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  content VARCHAR(255), 
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO posts (content) VALUES
+  ('i will hand down righteous judgment and smite all who disregard brumbpo tungus'),
+  ('watch as i wriggle in the swamp and love nge'),
+  ('transtrender voice: im gonna think deeply about worms'),
+  ('its time to ask the real questions: is it ok 2 avoid black sludge'),
+  ('back in my scene days i tried to work on my new book about kylie jenner?');
