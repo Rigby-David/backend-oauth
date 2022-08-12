153a016b84bef6ada17857a46c7de4d7e366abb3
David <Rigby335@hotmail.com>
2022-08-12 10:38:26 -0700

    /dashboard test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,16 +4,23 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/services/github');
+
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  
+  afterAll(() => {
+    pool.end();
+  });
+  
   it('should login and redirect users to /api/v1/github/dashboard', async () => {
     const res = await request
       .agent(app)
-      .get('/api/v1/call?code=42')
+      .get('/api/v1/github/callback?code=42')
       .redirects(1);
-
     expect(res.body).toEqual({
       id: expect.any(String),
       username: 'david_github_user',
@@ -23,7 +30,5 @@ describe('backend-express-template routes', () => {
       exp: expect.any(Number),
     });
   });
-  afterAll(() => {
-    pool.end();
-  });
+
 });
